a953ef6e6247920093f3c9886691d2662f9cd388
Lajos Santa <santa.lajos@gmail.com>
2018-12-02 17:03:11 +0100

    enh: tree: add PythonLoggerLeaf to public api

--- configpp/tree/__init__.py
+++ configpp/tree/__init__.py
@@ -3,4 +3,4 @@ from configpp.tree.items import NodeBase
 from configpp.tree.exceptions import ConfigTreeBuilderException
 from configpp.tree.item_factory import DictNodeFactory, LeafFactory
 from configpp.tree.settings import Settings
-from configpp.tree.custom_items import DatabaseLeaf
+from configpp.tree.custom_items import DatabaseLeaf, PythonLoggerLeaf
